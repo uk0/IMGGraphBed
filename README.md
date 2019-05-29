da40046bac365a2f71e3d02840454b081223068c
张建新 <zhangjianxin@bigsfintech.com>
2019-05-29 11:04:33 +0800

    release by vscode

---
+++ README.md
@@ -0,0 +1,18 @@
+
+### Esay Graph bed
+
+* Use HDFS
+* Use Qiniu
+* Use upyun
+* Use Local
+* Use some...
+
+
+### TODO
+* 水印
+* 隐写
+* 色情检测
+* 暴力检测
+* 账号体系等
+* Real shard 
+* ...
--- build.sh
+++ build.sh
@@ -1,2 +1,23 @@
 #!/usr/bin/env bash
 
+
+PASS=emhhbmdq111
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags static ./
+
+go build  -o mac_IMG -tags static ./
+
+
+# scp -i ~/Desktop/key/pvkey IMGGraphBed  root@10.50.40.10:/opt/
+# scp -i ~/Desktop/key/pvkey -r web  root@10.50.40.10:/opt/
+
+
+
+  sshpass -p ${PASS}  scp  IMGGraphBed  root@172.17.0.200:/opt/
+  sshpass -p ${PASS}  scp  -r web   root@172.17.0.200:/opt/
+
+PASS=Bigsfintech2018
+
+  sshpass -p ${PASS}  scp  IMGGraphBed  root@47.100.111.255:/opt/
+  sshpass -p ${PASS}  scp  -r web   root@47.100.111.255:/opt/
